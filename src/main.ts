b65784f248e77fa1ecc8fc62c9d334a6d6c4c856
MiniNec <55824895+MiniNec@users.noreply.github.com>
2023-09-22 14:54:04 +0800

    7 files modified, 4 added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
+import { createPinia } from 'pinia';
 import ElementPlus from 'element-plus';
+import router from './router';
 import 'element-plus/dist/index.css';
 
 const app = createApp(App);
-app.mount('#app');
+app.use(createPinia());
 app.use(ElementPlus);
+app.use(router);
+app.mount('#app');
---
+++ src/views/Index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <el-button>Default</el-button>
+  </div>
+</template>
+
+<style scoped></style>
