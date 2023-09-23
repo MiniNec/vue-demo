48c1c4b32062e7f95951ca1282f41173f6c64e9a
MiniNec <55824895+MiniNec@users.noreply.github.com>
2023-09-23 15:33:17 +0800

    3 files modified

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
+import Index from '@/views/Index.vue';
 </script>
 
 <template>
-  <div>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterView />
-  </div>
+  <!-- <RouterLink to="/">Home</RouterLink>
+    <RouterView /> -->
+  <index></index>
 </template>
 
 <style scoped></style>
