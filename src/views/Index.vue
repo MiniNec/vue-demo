ebf72f6175ba5ba6efbb304efff79f4e1b876dcc
MiniNec <55824895+MiniNec@users.noreply.github.com>
2023-09-23 22:07:02 +0800

    1 file modified, 1 added

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Place from '@/components/Place.vue';
+</script>
 
 <template>
   <div
@@ -9,59 +11,8 @@
       min-height: 100vh;
     "
   >
-    <ul>
-      <li><a>桃花源</a></li>
-      <li><a>参仙院</a></li>
-      <li><a>菊花台</a></li>
-    </ul>
+    <place name="123"></place>
   </div>
 </template>
 
-<style scoped>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: 'Oswald', sans-serif;
-}
-
-ul {
-  position: relative;
-  transform: skewY(-60deg);
-}
-
-ul li {
-  position: relative;
-  list-style: none;
-  width: 100px;
-  background: #eeeeee;
-  padding: 15px;
-  font-size: 25px;
-}
-
-ul li a {
-  text-decoration: none;
-  color: #999;
-  display: block;
-  text-transform: uppercase;
-  letter-spacing: 0.05em;
-  transition: 0.5s;
-  font-weight: 1000;
-}
-
-ul li:hover a {
-  color: #fff;
-}
-
-ul li::before {
-  content: '';
-  position: absolute;
-  top: 0;
-  left: -40px;
-  width: 40px;
-  height: 100%;
-  background: #d00;
-  transform-origin: right;
-  transform: skewY(45deg);
-}
-</style>
+<style scoped></style>
