488a6204cc315b532a4f959f5a2de1d04599f50e
MiniNec <55824895+MiniNec@users.noreply.github.com>
2023-09-27 16:46:01 +0800

    1 file modified, 1 added, 2 deleted

---
+++ src/components/Glass.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="spec-glass" :style="spec_style" :data-text="props.text"></div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{ rDeg: number; text: string }>();
+const spec_style = '--r:' + props.rDeg;
+console.log(spec_style, props.text);
+</script>
+
+<style scoped>
+.spec-glass {
+  position: relative;
+  width: 180px;
+  height: 240px;
+  background: linear-gradient(#fff2, transparent);
+  border: 1px solid rgba(255, 255, 255, 0.1);
+  box-shadow: 0 25px 25px rgba(0, 0, 0, 0.25);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: 0.5s;
+  border-radius: 10px;
+  transform: rotate(calc(var(--r) * 1deg));
+}
+
+.spec-glass::before {
+  content: attr(data-text);
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 40px;
+  background: rgba(255, 255, 255, 0.05);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  font-size: 25px;
+}
+</style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-import Demo from '@/components/Demo.vue';
-import LPlace from '@/components/LPlace.vue';
-import SPlace from '@/components/SPlace.vue';
+import Glass from '@/components/Glass.vue';
 </script>
 
 <template>
@@ -14,15 +12,27 @@ import SPlace from '@/components/SPlace.vue';
       scale: 1;
     "
   >
-    <sPlace name="123"></sPlace>
-    <!-- <lPlace name="1" style="--i: 9"></lPlace>
-    <lPlace name="2" style="--i: 8; margin-bottom: 90px"></lPlace>
-    <lPlace name="3" style="--i: 7; margin-bottom: 180px"></lPlace>
-    <lPlace name="4" style="--i: 6; margin-bottom: 270px"></lPlace>
-    <lPlace name="5" style="--i: 5; margin-bottom: 360px"></lPlace>
-    <lPlace name="6" style="--i: 4; margin-bottom: 450px"></lPlace>
-    <lPlace name="7" style="--i: 3; margin-bottom: 540px"></lPlace> -->
+    <div class="container">
+      <glass rDeg="0" text="镇江"></glass>
+      <glass rDeg="0" text="南京"></glass>
+      <glass rDeg="0" text="日本"></glass>
+      <glass rDeg="0" text="镇江"></glass>
+      <glass rDeg="0" text="南京"></glass>
+      <glass rDeg="0" text="日本"></glass>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  position: relative;
+  width: 1000px;
+  height: 1000px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: 0.5s;
+  border-radius: 10px;
+  background: #000;
+}
+</style>
