1db648f86fe9f54bcde40b79bd3114512e07bb74
MiniNec <55824895+MiniNec@users.noreply.github.com>
2023-10-13 08:54:47 +0800

    1 file modified, 2 added

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
 import { RouterLink, RouterView } from 'vue-router';
 import Index from '@/views/Index.vue';
 import Index2 from '@/views/Index2.vue';
+import TestScreen from '@/views/TestScreen.vue';
 </script>
 
 <template>
   <!-- <RouterLink to="/">Home</RouterLink>
     <RouterView /> -->
   <!-- <index></index> -->
-  <index2></index2>
+  <!-- <index2></index2> -->
+  <testScreen></testScreen>
 </template>
 
 <style scoped></style>
---
+++ src/views/TestScreen.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template><div>132</div></template>
+
+<style></style>
